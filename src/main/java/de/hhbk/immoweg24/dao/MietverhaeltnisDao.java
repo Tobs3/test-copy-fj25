8d950360b3a6e12772facc5695504f1ee0b217f5
adima <adimadis14@gmail.com>
2025-01-27 10:11:43 +0100

    MietverhaeltnisDao edited

--- src/main/java/de/hhbk/immoweg24/dao/MietverhaeltnisDao.java
+++ src/main/java/de/hhbk/immoweg24/dao/MietverhaeltnisDao.java
@@ -2,16 +2,17 @@ package de.hhbk.immoweg24.dao;
 
 import de.hhbk.immoweg24.model.Mietverhaeltnis;
 import de.hhbk.immoweg24.model.enums.StatusMietobjekt;
+import de.hhbk.immoweg24.model.enums.StatusMietverhaeltnis;
 
 public class MietverhaeltnisDao extends GenericDao<Mietverhaeltnis>{
     
     public MietverhaeltnisDao() { super(Mietverhaeltnis.class); }
     
-    public void deleteItemOnly(Mietverhaeltnis m) throws Exception {
+    public void MietobjektFreistellen(Mietverhaeltnis m) throws Exception {
         executeTransaction(session ->{
             Mietverhaeltnis mietverhaeltnis = session.get(Mietverhaeltnis.class, m.getId());
             if (mietverhaeltnis != null){
-                if (mietverhaeltnis.isGekuendigt()){
+                if (mietverhaeltnis.getMietobjekt().getStatus() != StatusMietobjekt.VERMIETET || mietverhaeltnis.getMietobjekt().getStatus() != StatusMietobjekt.INSTANDHALTUNG || mietverhaeltnis.getMietobjekt().getStatus() != StatusMietobjekt.RESERVIERT){
                     mietverhaeltnis.getMietobjekt().setStatus(StatusMietobjekt.VERFÜGBAR);
                 }
                 session.remove(mietverhaeltnis);
